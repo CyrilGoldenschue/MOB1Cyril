9091158240c00169380095dfb984b0c95249dacb
Cyril <cyril.goldenschue@cpnv.ch>
2021-05-28 15:11:34 +0200

    try  get token on localstorage

--- CSUNVB/screens/Common/HomeScreen.js
+++ CSUNVB/screens/Common/HomeScreen.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
 import { Button, View, Text } from 'react-native';
 import { createStackNavigator, createAppContainer } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+
+
+
+
 
 export default class Homescreen extends Component {
+  constructor(props){
+    super(props)
+          
+  }
+
   render() {
-    console.log(this.state)
+    /*AsyncStorage.getItem("user_token").then((value) => {})
+      .then(res => {
+          //do something else
+          this.setState({"user_token": res});
+          
+          
+      });*/
+    
+      
+    const token = () =>{ 
+      new Promise((resolve, reject) => {AsyncStorage.getItem("user_token")
+        .then(res => {
+          if (res !== null) {
+            resolve(res);
+          } else {
+            resolve(false);
+          }
+        })
+        .catch(err => reject(err));
+  });
+};
+        
+
+      
+    console.log(token)
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>About Screen</Text>
