9b29163e2b35265652fb48d29a0c3d6223e32f23
Cyril <cyril.goldenschue@cpnv.ch>
2021-06-10 15:08:28 +0200

    add display garde

---
+++ CSUNVB/components/consult.js
@@ -0,0 +1,126 @@
+import React, { Component } from "react";
+import { View, StyleSheet, Text, TextInput, TouchableOpacity } from "react-native";
+import Moment from 'moment';
+
+import APIKit from "../components/api"
+
+class ConsultationView extends Component {
+    constructor(props){
+        super(props)
+        this.state = {reportsGarde: [], reportsStup: []}
+        
+
+    }    
+
+
+    onSendReport(data){
+
+
+        const onSuccess = ({data}) => {
+            this.getReportsData()
+        };
+    
+        const onFailure = error => {
+            console.log(error && error.response);
+        };
+
+        let config = {
+            headers: {
+              'Authorization': 'Bearer ' + localStorage.getItem("user_token")
+            }
+          }
+        if(this.props.sort == "pharma"){
+            const {batch_id, drugsheet_id, date, start, end} = data
+            const payload = {batch_id, drugsheet_id, date, start, end}
+
+            payload.start = this.state.myNumber1 != "" ? this.state.myNumber1 : data.start == null ? onFailure : (data.start).toString()
+            payload.end = this.state.myNumber2 != "" ? this.state.myNumber2 :  data.end == null ? onFailure : (data.end).toString()
+            
+            APIKit.post('pharmacheck', payload, config)
+            .then(onSuccess)
+            .catch(onFailure);
+
+
+          }else if(this.props.sort == "nova"){
+            const {nova_id, drug_id, drugsheet_id, date, start, end} = data
+            const payload = {nova_id, drug_id, drugsheet_id, date, start, end}
+
+            
+            payload.start = this.state.myNumber1 != "" ? this.state.myNumber1 : (data.start).toString()
+            payload.end = this.state.myNumber2 != "" ? this.state.myNumber2 : (data.end).toString()
+            
+
+           APIKit.post('novacheck', payload, config)
+            .then(onSuccess)
+            .catch(onFailure);
+
+
+          }
+    }
+
+    
+
+    getReportsData() {
+    
+        let config = {
+            headers: {
+              'Authorization': 'Bearer ' + localStorage.getItem("user_token")
+            }
+          }
+        APIKit.get('reports/', config)
+            .then(res => {
+                const data = res.data
+                Moment.locale("fr")
+                const reportsGarde = data.shift.map(u =>
+                    <View key={ u.id } style={{marginBottom: 10}}>
+                        <Text style={{textAlign: 'left'}}>Pour le {Moment(u.date).format("Y-MM-D")} à {u.base}</Text>
+                    </View>
+                    
+                    )
+
+                    this.setState({
+                        reportsGarde: reportsGarde
+                    })
+
+                const reportsStup = data.drug.map(u =>
+                    <View key={ u.id } style={{marginBottom: 10}}>
+                        <Text style={{textAlign: 'left'}}>Le {Moment(u.date).format("D m YY")} à {u.base}</Text>
+                    </View>
+                    
+                    )
+
+                    this.setState({
+                        reportsStup: reportsStup
+                    })
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+    componentDidMount(){
+        this.getReportsData()
+    }
+  
+    render() {
+        return (
+            <>
+                { 
+                this.props.sort == "garde" ? this.state.reportsGarde : ""
+                }
+                {
+                this.props.sort == "stup" ? this.state.reportsStup : ""
+                }
+            </>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  titleReport: {
+      fontWeight: "bold",
+      fontSize: 18,
+  }
+  
+});
+
+export default ConsultationView;
--- CSUNVB/screens/Common/ConsultScreen.js
+++ CSUNVB/screens/Common/ConsultScreen.js
@@ -1,59 +1,60 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-let textBack = "<"
+import ConsultationView from "../../components/consult";
+
+let textBack = "<";
 
 export default class Reportscreen extends Component {
-  constructor(props){
-    super(props),
-    this.state = ({sort: ""})
+  constructor(props) {
+    super(props), (this.state = { sort: "" });
   }
-  
+
   render() {
     return (
       <View style={styles.container}>
-          <View style={styles.headerReport}>
-            <TouchableOpacity 
-              activeOpacity={0.95} 
-              style={styles.buttonGoBack} 
+        <View style={styles.headerConsult}>
+          <TouchableOpacity
+            activeOpacity={0.95}
+            style={styles.buttonGoBack}
+            onPress={() => {
+              localStorage.setItem("nav", "Home");
+              this.props.navigation.navigate("Home");
+            }}
+          >
+            <Text style={styles.textBack}>{textBack}</Text>
+          </TouchableOpacity>
+          <Text style={styles.title}>Consultation</Text>
+        </View>
+
+        <View style={styles.page}>
+          <Text style={styles.text}>Voir mes rapports de</Text>
+          <View style={styles.div}>
+            <TouchableOpacity
+              activeOpacity={0.95}
+              style={styles.buttonCheck}
               onPress={() => {
-                localStorage.setItem('nav', "Home");
-                this.props.navigation.navigate("Home")
-              }}>
-                <Text style={styles.textBack}>{textBack}</Text>
+                this.setState({ sort: "garde" });
+              }}
+            >
+              <Text style={styles.text}>Garde</Text>
             </TouchableOpacity>
-            <Text style={styles.title}>Consultation</Text>
+            <TouchableOpacity
+              activeOpacity={0.95}
+              style={styles.buttonCheck}
+              onPress={() => {
+                this.setState({ sort: "stup" });
+              }}
+            >
+              <Text style={styles.text}>Stup</Text>
+            </TouchableOpacity>
           </View>
-
-          <View style={styles.page}>
-            <Text style={styles.text}>Faire un</Text>
-            <View style={styles.div}>
-              <TouchableOpacity 
-                  activeOpacity={0.95} 
-                  style={styles.buttonCheck} 
-                  onPress={() => {
-                    this.setState({sort: "pharma"})
-                  }}>
-                    <Text style={styles.text}>Pharmacheck</Text>
-                </TouchableOpacity>
-                <TouchableOpacity 
-                  activeOpacity={0.95} 
-                  style={styles.buttonCheck} 
-                  onPress={() => {
-                    this.setState({sort: "nova"})
-                  }}>
-                    <Text style={styles.text}>NovaCheck</Text>
-                </TouchableOpacity>
-              </View>
-            <Text style={styles.text}>à {localStorage.getItem("baseName")}</Text>
-
-
-            
-
+          <View style={{marginLeft: 20, marginTop: 20}}>
+            <ConsultationView sort={this.state.sort} />
           </View>
-          
+        </View>
       </View>
-    )
+    );
   }
 }
 
@@ -63,51 +64,42 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     alignSelf: "stretch",
     textAlign: "center",
-    
   },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-    
-  },
-  
-  headerReport: {
-    flexDirection: 'row',
+  headerConsult: {
+    flexDirection: "row",
   },
   page: {
     flex: 3,
-    marginTop: 20
+    marginTop: 20,
+    justifyContent: "left",
   },
   div: {
-    flexDirection: 'row',
-    justifyContent: "center"
+    flexDirection: "row",
+    justifyContent: "center",
   },
-
-
 
   buttonGoBack: {
-    width: 50, 
-    height: 50, 
+    width: 50,
+    height: 50,
     marginLeft: "2%",
-    backgroundColor: 'rgb(33, 150, 243)',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "rgb(33, 150, 243)",
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: "2%",
-    elevation:3,
+    elevation: 3,
   },
-  buttonCheck:{
+  buttonCheck: {
     marginLeft: 10,
     marginRight: 10,
-    backgroundColor: 'rgb(33, 150, 243)',
-    width: 150, 
+    backgroundColor: "rgb(33, 150, 243)",
+    width: 150,
     height: 30,
   },
-
 
   text: {
     fontSize: 18,
     fontWeight: "bold",
+    marginBottom: 15
   },
   textBack: {
     fontSize: 50,
@@ -116,10 +108,6 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 50,
     marginLeft: "2%",
-    fontWeight: "bold"
-  }
-  
-  });
-  
-  
-  
+    fontWeight: "bold",
+  },
+});
